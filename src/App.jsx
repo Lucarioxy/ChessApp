54976380ff4113e1db8ea66ea13090c62553fee6
AnuprabhSingh <127375629+AnuprabhSingh@users.noreply.github.com>
2024-03-17 12:48:17 +0530

    Modal created

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 //import Board from './Board';
 import './App.css';
 import Board from './Components/chessBoard/Board';
+import Modal from './Components/Modal';
+import { useState } from 'react';
 
 function App() {
+    const [showModal,setShowModal] = useState(false)
     return(
-        <div id ="App">
-           <Board/>
-        </div>
+      <div className="h-screen flex flex-col items-center gap-6 bg-[#14161b]">
+        <h1 className="text-5xl font-bold mt-4 text-white"> Dummy Page</h1>
+        <button onClick={()=>setShowModal(true)} className="bg-violet-500 px-4 py-2 rounded-lg text-lg"> Create Game</button>
+      {showModal && <Modal onClose={()=> setShowModal(false)}/>}
+      </div>
     );
 }
 export default App;
