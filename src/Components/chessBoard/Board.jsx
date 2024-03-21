b250763fdd4edb5b6d968e6fc23cb4a21cde68b4
Nandiniranga <127603735+Nandiniranga@users.noreply.github.com>
2024-03-21 13:46:24 +0530

    added toast

--- src/Components/chessBoard/Board.jsx
+++ src/Components/chessBoard/Board.jsx
@@ -1,6 +1,8 @@
 import "./Board.css";
 import Tile from "./Tile/Tile";
 import Timer from './Timer';
+import { Bounce, ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
 const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
 
@@ -35,8 +37,33 @@ peices.push({image:"Assets/images/king_w.png",x:4,y:0})
 peices.push({image:"Assets/images/queen_w.png",x:3,y:0})
 
 
-
+const notify = () => {
+  toast.error('🦄 Wow so easy!', {
+    position: "top-center",
+    autoClose: false,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+    transition: Bounce,
+    });
+}
 export default function Board() { 
+  const notify = () => toast(
+   <div>
+    Wanna Resign?
+    <button className="bg-red-500 text-black rounded mx-8 px-6">YES</button>
+   </div>
+
+  );
+  const notify1 = () => toast(
+    <div>
+     DRAW
+    </div>
+ 
+   );
   let board = [];
 
   for (let j = verticalAxis.length - 1; j >= 0; j--) {
@@ -52,9 +79,25 @@ export default function Board() {
   }
 
   return<div>
-        <div id="Board" className="absolute px-96 h-52">{board}</div>
-        <div className = "absolute px-16 pt-96">
-          <Timer seconds={30}/> 
+        <div id="Board" className="place-items-center absolute pt-8 pb-8 px-96">{board}</div>
+        <div className="absolute px-8 py-12">
+          <Timer seconds={30}/>
         </div>
+        <div className="absolute inset-y-1 right-96 top-12">
+          <Timer seconds={30}/>
+        </div>
+        <button onClick = {notify} className="bg-red-500 rounded absolute bottom-2 right-4 px-12 text-black ">Resign</button>
+        <button onClick = {notify1} className="bg-green-700 rounded absolute bottom-2 right-40 px-12 text-black ">Draw</button>
+        <ToastContainer
+position="top-center"
+autoClose={false}
+newestOnTop={false}
+closeOnClick
+rtl={false}
+pauseOnFocusLoss
+draggable
+theme="light"
+transition =  {Bounce}
+/>
         </div>
 }
