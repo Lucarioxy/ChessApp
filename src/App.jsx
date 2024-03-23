8ae0ae4e2168ccb303b89e2328fe3cfcd429162c
Lucarioxy <122090878+Lucarioxy@users.noreply.github.com>
2024-03-23 19:19:09 +0530

    basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,21 @@ import GameEnd from './Components/Pages/GameEnd';
 import Waiting from './Components/Waiting';
 import { useState } from 'react';
 import HomePage from './Components/lobbypage/HomePage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { UserProvider } from '../context';
 
 function App() {
-    const [showModal,setShowModal] = useState(false)
-    return(
-      // <div className="h-screen flex flex-col items-center gap-6 bg-[#14161b]">
-      //   <h1 className="text-5xl font-bold mt-4 text-white"> Dummy Page</h1>
-      //   <button onClick={()=>setShowModal(true)} className="bg-violet-500 px-4 py-2 rounded-lg text-lg"> Create Game</button>
-      // {showModal && <Waiting onClose={()=> setShowModal(false)}/>}
-      // </div>
-      <Board/>
-    );
+  return (
+    <div className="App">
+      <UserProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/active-game" element={<Board />} />
+          </Routes>
+        </BrowserRouter>
+      </UserProvider>
+    </div>
+  );
 }
 export default App;
